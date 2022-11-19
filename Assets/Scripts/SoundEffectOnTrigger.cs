ccbb8999d581978b0531881e0a2f1c3fe8a0bb89
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-11-19 15:11:48 +0200

    Sound on trigger for gastric juice

---
+++ Assets/Scripts/SoundEffectOnTrigger.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SoundEffectOnTrigger : MonoBehaviour
+{
+    public AudioSource audioSourcePrefab;
+    public AudioClip clipOnEnter;
+    public AudioClip clipOnExit;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = Instantiate(audioSourcePrefab);
+    }
+    
+    private void PlaySound(AudioClip audioClip)
+    {
+        _audioSource.PlayOneShot(audioClip);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        var keyboardControl = other.GetComponent<PlayerKeyboardControl>();
+        if (keyboardControl != null)
+        {
+            PlaySound(clipOnEnter);
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        var keyboardControl = other.GetComponent<PlayerKeyboardControl>();
+        if (keyboardControl != null)
+        {
+            PlaySound(clipOnExit);
+        }
+    }
+}
