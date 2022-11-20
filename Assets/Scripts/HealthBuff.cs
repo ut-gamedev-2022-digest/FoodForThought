fcfef2ad4e80ad0f9c359d9a9e8c146f3abcb92a
BrandonRauba <brandonrauba00@gmail.com>
2022-11-20 13:51:33 +0200

    Health cap on heal

--- Assets/Scripts/HealthBuff.cs
+++ Assets/Scripts/HealthBuff.cs
@@ -8,7 +8,15 @@ public class HealthBuff : PowerupEffect
     public float amount;
     public override void Apply(GameObject target)
     {
-        target.GetComponent<Health>().health += amount;
-        target.GetComponent<Health>().slider.value = target.GetComponent<Health>().health + amount;
+        if (target.GetComponent<Health>().health + amount > 100)
+        {
+            target.GetComponent<Health>().health = 100;
+            target.GetComponent<Health>().slider.value = 100;
+        }
+        else
+        {
+            target.GetComponent<Health>().health += amount;
+            target.GetComponent<Health>().slider.value = target.GetComponent<Health>().health + amount;
+        }
     }
 }
