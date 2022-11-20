6139c9dc82df2e2ca882d2e1d5f32cb5aa7bf4bb
BrandonRauba <brandonrauba00@gmail.com>
2022-11-20 10:55:51 +0200

    Clicking sound

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,31 +5,37 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject PausePanel;
+    public AudioSource audioSource;
     public void PlayGame()
     {
+        audioSource.Play();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
     {
+        audioSource.Play();
         Application.Quit();
     }
     public void BackToMenu()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
         Time.timeScale = 1f;
+        audioSource.Play();
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1f;
         PausePanel.SetActive(false);
+        audioSource.Play();
     }
 
     public void RestartGame()
     {
+        Time.timeScale = 1f;
+        audioSource.Play();
         PausePanel.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        Time.timeScale = 1f;
     }
 }
